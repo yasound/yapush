6bf0474b1850ab0adbe4ac9391301b539a2dba4f
Jérôme Blondon <jerome@yasound.com>
2012-05-24 10:31:25 +0200

    added support for fabric

--- vtenv.sh
+++ vtenv.sh
@@ -13,6 +13,27 @@ then
     python virtualenv.py vtenv
 fi
 vtenv/bin/pip install -r requirements.txt --download-cache=eggs
+
+if [ ! -d ./vtenv/src/ssh ];then
+    vtenv/bin/pip install -e git://github.com/bitprophet/ssh.git#egg=ssh || echo "Please install python-dev and retry"
+else
+    cd vtenv/src/ssh
+    git pull origin master
+    cd ../../..
+fi
+
+if [ ! -d ./vtenv/src/fabric ];then
+    vtenv/bin/pip install -e git://github.com/fabric/fabric.git#egg=fabric
+else
+    cd vtenv/src/fabric
+    git pull origin master
+    cd ../../..
+fi
+if [ ! -f ./fab ];then
+    echo " * Now you can type ./fab prepare to init your dir"
+    ln -s vtenv/bin/fab fab
+fi
+
 echo " * Done"
 exit
 
