9d16b9ec0b9b210ecf1b170f3ca96390b55c19d1
Jérôme Blondon <jerome@yasound.com>
2012-05-24 11:18:50 +0200

    added logs at startup

--- yapush/run.py
+++ yapush/run.py
@@ -10,11 +10,12 @@ execfile(activate_this, dict(__file__=activate_this))
 from socketio.server import SocketIOServer
 from push import handle
 
+from logger import Logger
 
 if __name__ == "__main__":
     if settings.PRODUCTION_MODE:
-        print "Serving on https://%s:%s'" % (settings.HOST, settings.PORT)
+        Logger().log.info("Serving on https://%s:%s'" % (settings.HOST, settings.PORT))
         SocketIOServer((settings.HOST, settings.PORT), handle, policy_server=False, keyfile=settings.KEY_FILE, certfile=settings.CERT_FILE).serve_forever()
     else:
-        print "Serving on http://%s:%s'" % (settings.HOST, settings.PORT)
+        Logger().log.info("Serving on http://%s:%s'" % (settings.HOST, settings.PORT))
         SocketIOServer((settings.HOST, settings.PORT), handle, policy_server=False).serve_forever()
