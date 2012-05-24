329177360c16c550790af0a7e6d298d5e6e8c1c5
Jérôme Blondon <jerome@yasound.com>
2012-05-24 10:28:08 +0200

    increased log size to 10mb

--- yapush/push.py
+++ yapush/push.py
@@ -22,7 +22,7 @@ console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
 log.addHandler(console_handler)
 
-file_handler = RotatingFileHandler(settings.LOG_FILENAME, maxBytes=1024*10, backupCount=10)
+file_handler = RotatingFileHandler(settings.LOG_FILENAME, maxBytes=1024*10000, backupCount=10)
 file_handler.setFormatter(formatter)
 log.addHandler(file_handler)
 
